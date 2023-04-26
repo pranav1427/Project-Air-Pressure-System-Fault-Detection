1b8c246b3770c1f78042329e45d066158eb4af3d
pranav1427 <pranavvanage@gmail.com>
2023-04-26 16:14:43 +0530

    Data ingestion updated

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
 import sys ,os
+from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
 
-def test_logger_and_exception():
-     try:
-          result= 3/0
-          print(result)
-     except Exception as e:
-          raise SensorException(e, sys)
+
 
 if __name__== "__main__":
      try:
-          test_logger_and_exception()
+          training_pipeline_config=config_entity.TrainingPiplineConfig()
+          data_ingestion_config=config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
