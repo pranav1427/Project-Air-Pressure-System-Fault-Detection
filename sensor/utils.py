949fbd1e70d7857b3d5353c1a01ba47e44b9db21
pranav1427 <pranavvanage2gmail.com>
2023-04-27 13:22:12 +0530

    updated Data Validation

---
+++ sensor/utils.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+from sensor.config import mongo_client
+from sensor.logger import logging
+from sensor.exception import SensorException
+import os,sys
+import yaml
+
+def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    try:
+        """ Description: This function return collection as dataframe
+    =========================================================
+    Params:
+    database_name: database name
+    collection_name: collection name
+    =========================================================
+    return Pandas dataframe of a collection
+
+        """
+        logging.info(f"Reading data from database:{database_name} and collection {collection_name}")
+        df=pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Found columns:{df.columns}")
+        if "_id" in df.columns:
+            logging.info(f"dropping column :_id")
+            df= df.drop("_id",axis=1)
+        logging.info(f"row and col in df:{df.shape}")
+        return df
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir=os.path.dirname(file_path)
+
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
+
+    
