801c2769a9d6b2e1cb12dc6b62d49980f1f3e97d
pranav1427 <pranavvanage@gmail.com>
2023-04-29 12:35:30 +0530

    updated Model Trainer

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sensor.utils import get_collection_as_dataframe
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.model_trainer import ModelTrainer
+from sensor.components.data_transformation import DataTransformation
 
 
 
@@ -21,6 +23,15 @@ if __name__== "__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
           
           data_validation_artifact=data_validation.initiate_data_validation()
+
+          data_transformation_config=config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+          data_transformation=DataTransformation(data_transformation_config=data_transformation_config,
+          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation_artifact=data_transformation.initiate_data_transformation()
+
+          model_trainer_config=config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+          model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact=model_trainer.initiate_model_trainer()
 
      except Exception as e:
           print(e)
